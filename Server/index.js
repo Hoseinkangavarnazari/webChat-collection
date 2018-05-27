e99ca949aa9641ced023f13c6545fab84496f7d4
amirhosseinnazari <hnirsnazari@yahoo.com>
2018-05-28 02:44:54 +0430

    adding comments and refactoring

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,17 @@
 var server = require('ws').Server;
-
 var wss = new server({ port: 5001 });
+// creating server on port 5001 localhost 
 
 wss.on('connection', function (ws) {
 
+    //on message event 
     ws.on('message', (data) => {
         console.log(data);
-        var jsonObject = JSON.parse(data);
-        var username = jsonObject.name;
-        var message = jsonObject.message;
+        // var jsonObject = JSON.parse(data);
+        // var username = jsonObject.name;
+        // var message = jsonObject.message;
 
-        //broadcast
+        //broadcast data to all clients 
         wss.clients.forEach(function e(client) {
             client.send(data);
         })
--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,33 @@
 var ws = new WebSocket('ws://localhost:5001');
 
+
+// generating new id for every user
 function randomID() {
     return Math.floor(Math.random() * 1e11);
 }
 
+//set default name for nickname in ex scenarios
 var Nickname = "JOHN DOE";
 
 ws.onopen = function () {
-    console.log('connected');
+
+    console.log('Successfully connected to the server');
+
+    // get user ID if it's available or generating one
     var userID = localStorage.getItem("userID") || randomID();
+    //save userID in local storage of browser 
     localStorage.setItem("userID", userID);
 
+    // making reference to User Nickname with UserID 
     if (localStorage.getItem(userID + "NAME")) {
         Nickname = localStorage.getItem(userID + "NAME");
     } else {
+        // when there isn't any userID available get new Nickname from user 
         Nickname = prompt("What is your Nickname ?");
-        console.info(Nickname);
+
+        console.log("User nickname with userID " + userID + " is ", Nickname);
+
+        //set Nickname user in localstorage of browser 
         localStorage.setItem(userID + "NAME", Nickname);
     }
 }
@@ -23,11 +35,13 @@ ws.onopen = function () {
 ws.onmessage = function (data) {
 
     // here we should add new messages received from another clients 
-    console.log(data);
+    console.log("Data received from server (socket)", data);
+
+    //parse data
     var jsonObject = JSON.parse(data.data);
     var username = jsonObject.name;
     var message = jsonObject.message;
-
+    //make new dynamic post on messages box
     var message_container = document.createElement("div");
     message_container.id = "message";
     var name = document.createElement("b");
@@ -47,27 +61,31 @@ ws.onmessage = function (data) {
     messagesBox.appendChild(message_container);
 
     var objDiv = document.getElementById("messages");
+
+    // make sure always last message shown on messages box
     objDiv.scrollTop = objDiv.scrollHeight;
 }
 
 
-ws.onclose = function () {
-    console.log("closed websocket");
+ws.onclose = function (e) {
+    console.log("The connection with server lost : ", e);
 }
 
 
 function sendMessage() {
     var text = document.getElementById("message_input").value;
     document.getElementById("message_input").value = "";
-    if (text) {
 
+    //if the input is empty do nothing 
+    if (text) {
         var newMsg = {
             name: Nickname,
             message: text
         }
 
-        console.log(newMsg);
+        console.log("The message is : " + newMsg);
 
+        // we have to make strings for using send method on ws
         ws.send(JSON.stringify(newMsg));
     }
     else {
@@ -75,6 +93,9 @@ function sendMessage() {
     }
 }
 
+
+
+// send  message after pushing enter button
 var message_input = document.getElementById("message_input");
 message_input.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
