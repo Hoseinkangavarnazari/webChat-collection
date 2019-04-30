bbfba95b482963a948a3dcbe572d4c5ea13f7cdb
AmirHossein Nazari <hnirsnazari@yahoo.com>
2019-04-30 12:32:50 +0430

    fixing server html file

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,17 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static('assets'))
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../index.html'));
+});
+
+app.listen(8080);
+
+
 var server = require('ws').Server;
 var wss = new server({ port: 5001 });
 // creating server on port 5001 localhost 
